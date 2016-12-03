839e89ca8c573dc71a4d173abe07081b3216a63a
Mike Kennelly <mikekennellysignup@gmail.com>
2016-12-03 18:58:15 -0500

    Main Page final touches

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    
+    $('#login').click(function(){
+        window.location='search/index.html';
+    })
+    $('#register').click(function(){
+        window.location='search/index.html';
+    })
 })
